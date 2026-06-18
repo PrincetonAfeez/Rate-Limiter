235f712e6fe152d7f2771461c2760b4cf4c714b3
Princeton Afeez <princetonafeez@gmail.com>
2026-06-17 18:20:26 -0700

    Test fixtures for configs

---
+++ tests/fixtures/configs.py
@@ -0,0 +1,7 @@
+"""Test fixtures for configs."""
+
+TOKEN_RULE = "10/sec burst 20 algorithm token"
+FIXED_RULE = "100/min algorithm fixed-window"
+SLIDING_RULE = "50/min algorithm sliding-window-counter"
+LEAKY_RULE = "20/sec burst 40 algorithm leaky"
+
